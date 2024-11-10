d02f6dd7cf02ea0b941ae0d6cfeeb65028cb5e68
Musarrat Naz <147928780+Musarratnaz@users.noreply.github.com>
2024-11-10 12:08:56 +0500

    first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="h-[134px] bg-[#A29875]">
+      <h1 className='text-[#FFFFFF] text-[75px] h-[94px] w-[439px] mx-[36px] py-[20px] '>MANZZARI</h1>
+      
+        
+      
+    </div>
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,43 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen">
+      <Header />
+      <div className="flex h-[70%]">
+        <div className="w-1/2 gap-6 px-[176px] pt-[316px] flex flex-col justify-center items-start">
+          <h1 className="leading-[65.8px] text-[40px] font-bold text-[#000000] w-[496px] h-[189px]">
+            IMPECCABLE CRAFTSMANSHIP AND FINESSE
+          </h1>
+          <p className="text-[30px] font-medium leading-[49.35px] tracking-[0.025em] w-[902px] h-[147px]">
+            An example of intricate workmanship and detail, elegant necklaces
+            and long and short chains form a part of our desirable collection.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <aside className="flex flex-col mt-[198px] ml-[140px] w-[462px] h-[647px] ">
+          <div className="flex relative flex-col grow justify-center px-[24px] py-[28px] ">
+            <Image
+              loading="lazy"
+              src={"/images/slider-1.jpg.svg"}
+              width={490}
+              height={667}
+              alt="Elegant jewelry display"
+              className="absolute inset-0 size-full rounded-[150px_0px_150px_0px]"
+            />
+            <div className="flex overflow-hidden relative flex-col border border-white border-solid rounded-[150px_0px_150px_0px]">
+              <div className="flex shrink-0 border border-white border-solid w-[442px] h-[611px] rounded-[150px_0px_150px_0px]"></div>
+            </div>
+          </div>
+                  
+        </aside>
+      </div>
+      <div className="w-[228px] h-[58px] pl-[20px] ml-[400px] bg-[#A29875] p-[10px] gap-[15px] rounded-[10px]">
+        <h1
+          className="w-[177px] h-[38px] text-[#FFFFFF] text-[30px]">
+          Explore Now
+        </h1>
+      </div>
     </div>
   );
 }
